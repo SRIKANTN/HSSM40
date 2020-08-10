e793be98e7a631b2300039ef5decf27bd672e21a
Binny <Binny@DESKTOP-280B4F5>
2020-08-10 08:28:38 +0530

    2nd file

---
+++ src/Com_ActiTime_Test/DemoA.java
@@ -0,0 +1,14 @@
+package Com_ActiTime_Test;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoA 
+{
+	@Test
+	public void testA()
+	{
+		Reporter.log("testA...",true);
+	}
+
+}
